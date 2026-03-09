33653e2f1c9a789c62b386bbd1fab05ffc250f24
deoraja <deoraja9@gmail.com>
2026-03-09 21:17:44 +0530

    Add latency benchmarking for cache vs database performance

--- benchmark/load_test.py
+++ benchmark/load_test.py
@@ -1,9 +1,21 @@
 from cache.cache_service import CacheService
-
+import time
 
 service = CacheService()
 
-print(service.get_data(1))
-print(service.get_data(1))
-print(service.get_data(2))
-print(service.get_data(1))
+start = time.time()
+service.get_data(1)
+end = time.time()
+
+db_time = end - start
+print("\nDB Response Time:", db_time)
+
+start = time.time()
+service.get_data(1)
+end = time.time()
+
+cache_time = end - start
+print("\nCache Response Time:", cache_time)
+
+speedup = db_time / cache_time
+print("\nCache is", round(speedup,2),"x faster than DB")
